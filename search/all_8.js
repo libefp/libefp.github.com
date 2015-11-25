361d5ad7c964278108ce08a0e0dcc5a2ea53becb
Ilya Kaliman <ilya.kaliman@gmail.com>
2015-11-24 18:14:20 -0800

    web

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['the_20effective_20fragment_20potential_20method_20implementation',['The Effective Fragment Potential method implementation',['../index.html',1,'']]],
+  ['the_20effective_20fragment_20potential_20method',['The Effective Fragment Potential method',['../index.html',1,'']]],
   ['terms',['terms',['../structefp__opts.html#a4ec4f0243a7e31d41130adc5d9989599',1,'efp_opts']]],
   ['total',['total',['../structefp__energy.html#a0db3434685baa95c90385830abc16473',1,'efp_energy']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['the_20effective_20fragment_20potential_20method_20implementation',['The Effective Fragment Potential method implementation',['../index.html',1,'']]]
+  ['the_20effective_20fragment_20potential_20method',['The Effective Fragment Potential method',['../index.html',1,'']]]
 ];
